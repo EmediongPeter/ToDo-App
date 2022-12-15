70a9ac0f0b6e5dd4378de80b699c25e9e7cac594
EmediongPeter <emedionguseh18@gmail.com>
2022-12-15 12:20:21 +0100

    first commit

---
+++ db/connect.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const connectDB = (url) => {
+  mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  console.log("Connected to DB")
+  return
+}
+
+module.exports = connectDB
